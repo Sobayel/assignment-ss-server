df35e410602d0f7c259cc8796d494d7df43f5f66
mdsobayelhossain <mdsobayelhossain@gmail.com>
2024-05-12 19:39:57 +0600

    update server site

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xfjzvlh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -75,7 +74,7 @@ async function run() {
 
 
 
-    // Send a ping to confirm a successful connection
+
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
