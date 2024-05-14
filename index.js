66181e31b897a5f0b6dc3ba7c4d1c29c5dfaa6e9
mdsobayelhossain <mdsobayelhossain@gmail.com>
2024-05-14 23:48:14 +0600

    added cookie token

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken')
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+const corsData ={
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
 
+app.use(cors(corsData));
+app.use(express.json());
+app.use(cookieParser())
+
+const logger = async(req, res, next) => {
+  console.log('called',req.host, req.originalUrl)
+  next();
+}
+
+
+const verifyToken = (req, res, next) =>{
+  const token = req.cookies?.token
+  if(!token) {
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        if(err) {
+          console.log(err)
+          return res.status(401).send({message: 'unauthorized access'})
+        }
+        console.log(decoded)
+        req.user = decoded
+        next()
+      })
+}
 
 
 
@@ -21,6 +54,12 @@ const client = new MongoClient(uri, {
   }
 });
 
+cookieOption ={
+  httpOnly:true,
+  secure:process.env.NODE_ENV === "production"? true: false,
+  sameSite: process.env.NODE_ENV === "production"? "none":"strict",
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -28,25 +67,42 @@ async function run() {
 
     const assignmentCollection = client.db("skillUpDB").collection('skillUp');
     const pdfCollection = client.db("skillUpDB").collection('pdf');
-    const SubmitAssignmentCollection = client.db("skillUpDB").collection('submitAssignment');
+    const submitAssignmentCollection = client.db("skillUpDB").collection('submitAssignment');
 
 
-    app.get('/assignment/pending', async(req, res) =>{
-         const result =await pdfCollection.find({status:"pending"}).toArray();
-        console.log(result)
-        res.send(result);
-    })
+    app.post('/jwt',logger, async(req, res) =>{
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '24h'})
+      res
+      .cookie('token',token,cookieOption)
+      .send({success: true});
+  })
+    
+
+  app.post('/logOut', async (req, res) => {
+    const user = req.body;
+    console.log('logged in user',user)
+    res.clearCookie('token', {...cookieOption,maxAge: 0, sameSite:'none',secure:true}).send({success: true});
+})
 
     app.get('/my-assignment', async(req, res) =>{
-        const email= req.query.email
-        const result = await SubmitAssignmentCollection.find({email }).toArray();
+        const result = await submitAssignmentCollection.find().toArray();
         res.send(result);
     })
+
     app.post('/my-assignment', async(req, res) =>{
         const data = req.body;
-        const result = await pdfCollection.insertOne(data);
+        console.log(data);
+        const result = await submitAssignmentCollection.insertOne(data);
         res.send(result);
     })
+
+    app.get('/pdf', async(req, res) =>{
+         const result =await pdfCollection.find().toArray();
+        res.send(result);
+    })
+
     app.post('/pdf', async(req, res) =>{
         const newPdf = req.body;
         const result = await pdfCollection.insertOne(newPdf);
